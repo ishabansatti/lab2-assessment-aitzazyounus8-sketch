9e22b532cb4e759f544ab4cff12678d90611dc66
aitzazyounus8-sketch <aitzazyounus8@gmail.com>
2026-02-18 17:35:20 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -6,15 +6,15 @@ int getIntValue() {
     int var_int = 10;
     return var_int;
 }
-// hey
+
 float getFloatValue() {
-    // Declare a variable var_float of type float and set its value to 10.75.
-    //return var_float;
+    int var_int = 10.75;
+    return var_float;
 }
 
 char getCharValue() {
-    // Declare a variable var_char of type character and set its value to A.
-    //return var_char;
+    char var_char='A'
+    return var_char;
 }
 
 int convertFloatToInt(float value) {
